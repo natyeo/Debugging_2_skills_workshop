27ff68b5657758365bbe3727c7af4b9d482fdda0
Natalie <49326857+natyeo@users.noreply.github.com>
2019-11-07 12:23:04 +0000

    all tests passing

---
+++ lib/app.rb
@@ -0,0 +1,22 @@
+require 'sinatra/base'
+require_relative "./struggle_table_flipper"
+require_relative "./random_happy_emoji"
+
+class StruggleTableFlipperApp < Sinatra::Base
+
+  get '/' do
+    redirect '/emoji'
+  end
+
+  get "/emoji" do
+    @emoji =  RandomHappyEmoji.new
+    erb :index
+  end
+
+  post "/flipped_struggle" do
+    @flipped_struggle = StruggleTableFlipper.new(params[:struggle]).flipped_struggle
+    erb :flipped_struggle
+  end
+
+  run! if app_file == $0
+end
